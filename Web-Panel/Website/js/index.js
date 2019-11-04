b0d61e62092e360a97bebb41648fedc3e23231d9
clement-soler1 <47113650+clement-soler1@users.noreply.github.com>
2019-11-04 16:53:41 +0100

    Ajout website V0.1

---
+++ Web-Panel/Website/js/index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+	var options = false;
+    var elems1 = document.querySelectorAll('.sidenav');
+    var instances1 = M.Sidenav.init(elems1, options);
+});
+
+/*
+$(document).ready(function() {
+	$('.parallax').parallax();
+  	$('.sidenav').sidenav();
+});*/
