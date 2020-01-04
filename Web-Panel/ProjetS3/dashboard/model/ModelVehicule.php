ba64b5bf7ea2c36b929f2bfa34f432619110d424
clement-soler1 <clement.soler@etu.umontpellier.fr>
2020-01-04 14:59:41 +0100

    Fix updater de vehicule + vue des spé du vehicule

--- Web-Panel/ProjetS3/dashboard/model/ModelVehicule.php
+++ Web-Panel/ProjetS3/dashboard/model/ModelVehicule.php
@@ -60,6 +60,83 @@ class ModelVehicule {
         
     }
 
+    public static function getVehicleById($idv) {
+
+        $sql = "SELECT * FROM Ap_Vehicule WHERE idVehicule=:tag_idv;";
+
+        $req_prep = Model::$pdo->prepare($sql);
+
+        $values = array(
+            "tag_idv" => $idv,
+        );
+
+        $req_prep->execute($values);
+
+        $req_prep->setFetchMode(PDO::FETCH_CLASS, 'ModelVehicule');
+        $tab = $req_prep->fetchAll();
+
+        return $tab[0];
+
+    }
+
+    /**
+     * @return null
+     */
+    public function getMarque()
+    {
+        return $this->marque;
+    }
+
+    /**
+     * @return null
+     */
+    public function getModele()
+    {
+        return $this->modele;
+    }
+
+    /**
+     * @return null
+     */
+    public function getCouleur()
+    {
+        return $this->couleur;
+    }
+
+    public function printImmat()
+    {
+        $immat = $this->immatriculation;
+        $str = str_split($immat, 2);
+        $str2 = str_split($str[2]);
+
+        $formatedImmat = "" . $str[0] . "-" . $str[1] . $str2[0] . "-" . $str2[1] . $str[3];
+        return $formatedImmat;
+    }
+
+    public function printAuthorizedPerson() {
+        $sql = "SELECT prenom,nom FROM Ap_User APU JOIN"
+            . " Ap_PossesVehicule APV ON APV.idUser=APU.idUser WHERE idVehicule=:tag_idv;";
+
+        $req_prep = Model::$pdo->prepare($sql);
+
+        $values = array(
+            "tag_idv" => $this->idVehicule,
+        );
+
+        $req_prep->execute($values);
+        $tab = $req_prep->fetchAll();
+
+        $strReturn = " ";
+
+        foreach ($tab as $val) {
+            $strReturn = $strReturn . ucfirst($val["prenom"]). " " . ucfirst($val["nom"]) . ', ';
+        }
+
+        return (substr($strReturn, 0, -2));
+    }
+
+
+
 
     
     
--- Web-Panel/ProjetS3/dashboard/view/dashboard/vehicule.php
+++ Web-Panel/ProjetS3/dashboard/view/dashboard/vehicule.php
@@ -2,7 +2,24 @@
 
 <div class="vehContainerMain">
 
-    <div class="centeredCard">
-    </div>
+    <br><br><br><br><br>
+    <?php
+        $veh = ModelVehicule::getVehicleById($_SESSION['idv']);
+
+        echo '<div class="centeredCard">';
+            echo '<h1 class="txtCentered">'. $veh->getSurnom() .'</h1>';
+        echo '</div>';
+        echo '<div class="centeredCard2">';
+
+            echo '<p class="txtCentered"><b>Marque : </b>'. $veh->getMarque() .'</p>';
+            echo '<p class="txtCentered"><b>Modèle : </b>'. $veh->getModele() .'</p>';
+            echo '<p class="txtCentered"><b>Couleur : </b>'. $veh->getCouleur() .'</p>';
+            echo '<br>';
+            echo '<p class="txtCentered"><b>Immatriculation : </b>'. $veh->printImmat() .'</p>';
+            echo '<br>';
+            echo '<p class="txtCentered"><b>Personnes autorisées :</b></p>';
+            echo '<p class="txtCentered">'. $veh->printAuthorizedPerson() .'</p>';
+        echo '</div>';
+    ?>
 
 </div>
