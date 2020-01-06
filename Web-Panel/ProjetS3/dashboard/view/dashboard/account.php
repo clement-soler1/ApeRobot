a47326bc5356200bf7fc1cdad33589bcce504c71
Clément Soler <clement.soler@etu.umontpellier.fr>
2020-01-06 15:25:36 +0100

    debut page acct

--- Web-Panel/ProjetS3/dashboard/view/dashboard/account.php
+++ Web-Panel/ProjetS3/dashboard/view/dashboard/account.php
@@ -1,1 +1,15 @@
-<a>Déconnexion</a>
+<link rel="stylesheet" type="text/css" href="./view/css/acct.css">
+
+<div class="account-card">
+    <h1 class="acct-name">Clément Soler</h1>
+    <p><b>E-mail :</b> clement.soler@etu.umontpellier.fr</p>
+    <p><b>Date de Naissance :</b> 06/10/2000</p>
+    <br>
+    <p><b>Véhicules créés</b></p>
+    <p>AlfaRoméo Mito</p>
+    <br>
+    <p><b>Véhicules Accesibles</b></p>
+    <p>AlfaRoméo Mito</p>
+    <br>
+    <a>Déconnexion</a>
+</div>
